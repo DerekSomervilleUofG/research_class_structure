5e6e9fef0035d31dc63b0183226c0c192556f193
Derek Somerville <derek.somerville@glasgow.ac.uk>
2025-07-16 09:04:07 +0100

    Added DeveloperExperience

---
+++ class_structure/DeveloperExperience.py
@@ -0,0 +1,17 @@
+from class_structure.Structure import Structure
+from datetime import datetime
+class DeveloperExperience(Structure):
+
+    local_control_populate = LocalControlPopulate()
+    input_format = "%Y-%m-%dT%H:%M:%SZ"
+    output_format = "%Y%m%d%H%M%S"
+
+    def __init__(self, developer, repository_name, date, language):
+        super().__init__(repository_name)
+        self.developer = developer
+        self.pull_date = datetime.strptime(date, self.input_format)
+        self.language = language
+
+    def get_pull_date(self):
+        return self.pull_date.strftime(self.output_format)
+
